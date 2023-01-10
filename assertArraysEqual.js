2ee7d97f7a4c5a487f67c78fc898ef709e00bf7b
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-01-10 19:53:04 +0000

    add util.inspect to change printed array into string

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const util = require("util");
+
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
     return false;
@@ -14,8 +16,16 @@ const eqArrays = (arr1, arr2) => {
 
 const assertArraysEqual = (arr1, arr2) => {
   eqArrays(arr1, arr2)
-    ? console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`)
-    : console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+    ? console.log(
+        `✅✅✅ Assertion Passed: [${util.inspect(arr1)}] === [${util.inspect(
+          arr2
+        )}]`
+      )
+    : console.log(
+        `🛑🛑🛑 Assertion Failed: [${util.inspect(arr1)}] !== [${util.inspect(
+          arr2
+        )}]`
+      );
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, "3"]);
