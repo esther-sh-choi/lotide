e569209c985f687cd0ac354d2e5b8a758b7b6ff7
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-01-11 22:51:50 +0000

    implement the findKey function

---
+++ findKey.js
@@ -0,0 +1,53 @@
+const assertEqual = (actual, expected) => {
+  actual === expected
+    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKey = (object, callback) => {
+  for (const [key, value] of Object.entries(object)) {
+    if (callback(value)) {
+      return key;
+    }
+  }
+};
+
+const result1 = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+);
+
+const result2 = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 1
+);
+
+const result3 = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 0
+);
+
+assertEqual(result1, "noma");
+assertEqual(result2, "Blue Hill");
+assertEqual(result3, undefined);
