d20f43ea2066d4f1b02fa523b204b46fc8ea21ad
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-01-11 22:44:43 +0000

    implement the takeUntil function

---
+++ takeUntil.js
@@ -0,0 +1,68 @@
+const util = require("util");
+
+const eqArrays = (arr1, arr2) => {
+  // if arrays are not the same length, they are not equal
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  // if the function didn't return a false so far, it will automatically return true
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  // add util.inspect so that the arrays will be convered to string type before being printed to the console
+  // this is crucial to keeps the quotes on the string elements in the console
+  /* eslint-disable */
+  eqArrays(arr1, arr2)
+    ? console.log(
+        `✅✅✅ Assertion Passed: ${util.inspect(arr1)} === ${util.inspect(
+          arr2
+        )}`
+      )
+    : console.log(
+        `🛑🛑🛑 Assertion Failed: ${util.inspect(arr1)} !== ${util.inspect(
+          arr2
+        )}`
+      );
+  /* eslint-enable */
+};
+
+const takeUntil = (array, callback) => {
+  let result = [];
+  for (const item of array) {
+    if (callback(item)) {
+      break;
+    }
+    result.push(item);
+  }
+  return result;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, (x) => x < 0);
+
+console.log("---");
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood",
+];
+const results2 = takeUntil(data2, (x) => x === ",");
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+// prettier-ignore
+assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
