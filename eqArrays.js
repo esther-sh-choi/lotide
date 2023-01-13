2661ef621be371710e13f0a14c5a436098390fbb
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-01-13 17:50:45 +0000

    Write eqArrays function recursively

--- eqArrays.js
+++ eqArrays.js
@@ -5,12 +5,19 @@ const assertEqual = (actual, expected) => {
 };
 
 const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
+  if (arr1.length !== arr2.length || typeof arr1 !== typeof arr2) {
     return false;
   }
 
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+  const sortedArr1 = [...arr1].sort();
+  const sortedArr2 = [...arr2].sort();
+
+  for (const i in sortedArr1) {
+    if (Array.isArray(sortedArr1[i]) || Array.isArray(sortedArr2[i])) {
+      if (!eqArrays(sortedArr1[i], sortedArr2[i])) {
+        return false;
+      }
+    } else if (sortedArr1[i] !== sortedArr2[i]) {
       return false;
     }
   }
@@ -18,10 +25,23 @@ const eqArrays = (arr1, arr2) => {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+
+console.log(
+  eqArrays(
+    [[2, 3], [4]],
+    [
+      [2, 3],
+      [4, 5],
+    ]
+  )
+); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
