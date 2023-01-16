310d1c602b347110b18e1cf388ae7423a80068e7
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-01-16 15:15:21 +0000

    Refactor tailTest and middleTest using Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,15 @@
-const assertArraysEqual = require("../assertArraysEqual");
 const tail = require("../tail");
+const assert = require("chai").assert;
 
-assertArraysEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertArraysEqual(result, ["Lighthouse", "Labs"]);
+describe("#tail", () => {
+  it("returns [2, 3, 4, 5] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
+  it('returns ["Hello", "Lighthouse", "Labs"] for ["Lighthouse", "Labs"]', () => {
+    const result = tail(
+      ["Hello", "Lighthouse", "Labs"],
+      ["Lighthouse", "Labs"]
+    );
+    assert.deepEqual(result, ["Lighthouse", "Labs"]);
+  });
+});
